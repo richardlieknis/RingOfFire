c8b055aeca86fa4b8b9126c9fdf377562d85f7d1
Richard Lieknis <richard.lieknis@web.de>
2023-03-22 21:37:25 +0100

    player added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GameComponent } from './game/game.component';
+import { PlayerComponent } from './player/player.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
 
 const routes: Routes = [
   {path: '', component: StartScreenComponent},
   {path: 'game', component: GameComponent },
+  {path: '', component: PlayerComponent },
 ];
 
 @NgModule({
