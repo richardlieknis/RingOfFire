d17240692c5538d0a6fa99f075941201a6ebda02
Richard Lieknis <richard.lieknis@web.de>
2023-03-22 21:09:00 +0100

    pick cards animation added

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
+
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game;
+
+  constructor(){}
+
+  ngOnInit(): void {
+      this.game = new Game();
+      console.log(this.game);
+  }
+  newGame(){
+      this.game = new Game();
+  }
+  takeCard(){
+    if (!this.pickCardAnimation){
+      this.currentCard = this.game.stack.pop();
+     
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+         this.game.playedCard.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 2500 )
+    }
+  }
+
+}
